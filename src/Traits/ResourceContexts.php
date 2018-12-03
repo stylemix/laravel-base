f11edcc41a4c4cc0e01997a2985863bb64d52cb3
Azamat <azamat@stylemix.net>
2018-12-03 18:47:49 +0500

    Api resource context trait.

---
+++ src/Traits/ResourceContexts.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Stylemix\Base\Traits;
+
+trait ResourceContexts
+{
+
+	/**
+	 * Get array for current contextual request
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 *
+	 * @return mixed If context is not defined then false is returned
+	 */
+	protected function toContextArray($request)
+	{
+		$context = $this->getContext($request) ?: 'default';
+
+		if (method_exists($this, $method = $context . 'Context')) {
+			return $this->$method($request);
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get current context
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 *
+	 * @return mixed
+	 */
+	protected function getContext($request)
+	{
+		if (property_exists($this, 'context')) {
+			return $this->context;
+		}
+
+		return $request->get('context');
+	}
+
+	/**
+	 * Check current context
+	 *
+	 * @param string $context
+	 * @param \Illuminate\Http\Request $request
+	 *
+	 * @return bool
+	 */
+	protected function isContext($context, $request)
+	{
+		return $this->getContext($request) == $context;
+	}
+
+	protected function defaultContext($request)
+	{
+		return parent::toArray($request);
+	}
+}
